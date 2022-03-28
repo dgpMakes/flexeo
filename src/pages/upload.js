270d48a1c09bc72a88b35a8988332f05e534f5e7
dgpMakes <dgpmakes@gmail.com>
2022-03-28 19:48:48 +0200

    Revert "Divided pages. Created welcome page"
    This reverts commit 943b11cb4e761f46cb958e355ce51f54b1dba1ae.

--- src/pages/upload.js
+++ src/pages/upload.js
@@ -3,16 +3,11 @@ import React from 'react';
 import UploadForm from '../components/UploadForm';
 import ProductForm from '../components/ProductForm';
 import MixForm from '../components/MixForm';
-import NavbarUser from '../components/NavbarUser';
-import { SuperContainer } from './PagesElements';
 
 const Upload = () => {
     return (
         <>
-            <NavbarUser />
-            <SuperContainer>
-                <MixForm/>
-            </SuperContainer>
+            <MixForm/>    
         </>
     );
 };
