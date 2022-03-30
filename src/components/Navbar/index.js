42b036bc82acc3ff0ddd702f162e1e6b34e37d6d
Diego G <dgpmakes@gmail.com>
2022-03-30 11:31:50 +0200

    Parsing cookie

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,9 +13,17 @@ import LoginButton from '../LoginButton';
 
 function getCookie(key) {
   var b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
-  key = b[0];
-  key = key.substring(8);
-  console.log(key);
+  token = b[0];
+  token = token.substring(8);
+  var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    console.log(JSON.parse(jsonPayload));
+
+    //return JSON.parse(jsonPayload);
 }
 
 const Navbar = () => {
